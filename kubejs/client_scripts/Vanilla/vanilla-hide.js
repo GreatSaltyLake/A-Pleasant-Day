77b031949e2d92beea530b648135044bafa92d3e
GreatSaltyLake <102194736+GreatSaltyLake@users.noreply.github.com>
2024-08-06 21:09:36 -0400

    v0.1.5

--- kubejs/client_scripts/Vanilla/vanilla-hide.js
+++ kubejs/client_scripts/Vanilla/vanilla-hide.js
@@ -12,24 +12,4 @@ JEIEvents.hideItems((apd) => {
         'minecraft:stone_shovel',
         'minecraft:stone_sword'
     ])
-    // Tropicraft items
-    apd.hide([
-        'tropicraft:mud',
-	    'tropicraft:mud_with_pianguas',
-        'tropicraft:zircon_hoe',
-        'tropicraft:zircon_axe',
-        'tropicraft:zircon_shovel',
-        'tropicraft:zircon_pickaxe',
-        'tropicraft:zircon_sword',
-        'tropicraft:zirconium_hoe',
-        'tropicraft:zirconium_axe',
-        'tropicraft:zirconium_shovel',
-        'tropicraft:zirconium_pickaxe',
-        'tropicraft:zirconium_sword',
-        'tropicraft:eudialyte_hoe',
-        'tropicraft:eudialyte_axe',
-        'tropicraft:eudialyte_shovel',
-        'tropicraft:eudialyte_pickaxe',
-        'tropicraft:eudialyte_sword',        
-    ])
 })
